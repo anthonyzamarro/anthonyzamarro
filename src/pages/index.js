c56adfcfae03f21c0ed75cd011f94ca2ea308cfd
Anthony Zamarro <anthonyzamarro@Anthonys-MacBook-Air.local>
2020-06-24 22:01:26 -0400

    add starter blog posts, mdx, and basic blog template

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,18 +2,19 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+// import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    <h1>Tonyz's Blog</h1>
+    <p>On this blog I write mostly about tech related topics to better understand them.</p>
+    <ul>
+      <li><Link to="/2020/06-24--ssh">SSH</Link></li>
+      <li><Link to="/2020/06-25--cookies-sessions-local-storage">Cookies vs. Sessions vs. Local Storage</Link></li>
+      <li><Link to="/2020/06-26--ssl-tls">SSL/TLS</Link></li>
+    </ul>
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
